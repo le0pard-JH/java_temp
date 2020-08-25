6c1523a1bcb4db9d5c9c92e2fd3b7b5cff6bbe89
harkn <harkn@JH-MSI>
2020-08-25 12:41:18 +0900

    This is JAVA Git Push Test

---
+++ java_temp/src/java_temp/java_temp_01.java
@@ -0,0 +1,12 @@
+package java_temp;
+
+public class java_temp_01 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		System.out.println("JAVA GIT PUSH TEST");
+
+	}
+
+}
